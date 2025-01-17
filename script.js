ce5be5bb78f2457a8255b3bf5dd059d630a0d61d
KHADIJA-09 <132650876+KHADIJA-09@users.noreply.github.com>
2025-01-17 20:32:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-//complete this code
 class Person {
-	constructor(name,age)
-	{
-		this._name = name
-		this._age = age
-	}
-	get  fname()
-	{
-		console.log(this.name)
-	}
-	set  fage(age)
-	{
-		
-	if (value < 0) {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(value) {
+    if (value < 0) {
       throw new Error("Age cannot be negative.");
     }
     this._age = value;
   }
-	}
 }
 
 class Student extends Person {
-	study()
-	{
-		console.log(`${this.name} is studying`)
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-  teach()
-	{
-		console.log(`${this.name} is teaching`)
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
